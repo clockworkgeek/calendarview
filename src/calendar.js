53ee53b5f93463d63f9a8a5960e5b5a6e95ec9a9
Daniel Deady <daniel@clockworkgeek.com>
2014-08-05 21:24:24 +0100

    First impression of version 2.0
    - Architecture is modular. Completely rewritten from scratch.
    - Variables are renamed, not backward compatible.
    - Behaviors are mixins instead of configuration.
    - Charset is utf-8 (smaller files).
    - Locale is more concise and used for more translation.

---
+++ src/calendar.js
@@ -0,0 +1,229 @@
+/**
+ *  This file is part of CalendarView.
+ *
+ *  CalendarView is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CalendarView is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with CalendarView.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @copyright 2014 Daniel Deady <daniel@clockworkgeek.com>
+ *  
+ *  Based on previous work by, with thanks to, but without copying of:
+ *  - Yuri Leikind
+ *  - Justin Mecham <justin@aspect.net>
+ *  - Mihai Bazon (author of original www.dynarch.com/projects/calendar)
+ */
+
+(function(){
+    'use strict';
+
+    self.Calendar = Class.create(Widget, { /** @memberOf Calendar */
+        selectClass: 'selected',
+        nowClass: 'today',
+
+        initializeElements: function($super) {
+            $super();
+            this.update(this.date || this.getValue());
+        },
+
+        getValue: function($super) {
+            return $super() || new Date;
+        },
+
+        /*
+         * Controls should call when updating appropriate element.
+         * Other mixins should override to make modifications.
+         */
+        updatePeriod: function(date, range, element) {
+            var value = range.include(this.date = date);
+            element
+                .toggleClassName(this.nowClass, range.include(new Date))
+                .toggleClassName(this.selectClass, value)
+                .selected = value;
+        }
+
+    });
+
+})();
+
+(function() {
+
+    Element.addMethods({
+        addTag: function(element, tagName, attributes) {
+            return element.appendChild(new Element(tagName, attributes));
+        }
+    });
+
+    /*
+     * Widget control mixin.
+     * 
+     * Single month displayed as a table.
+     */
+    Calendar.Dates = { /** @memberOf Calendar.Dates */
+        firstDayOfWeek: 0,
+
+        getContent: function($super) {
+            var content = $super();
+            var table = this.datesTable = content.addTag('table'),
+                thead = table.addTag('thead'),
+                tbody = table.addTag('tbody'),
+                days = this.locale.get('day_abbrs');
+            table.insert('<tfoot><tr><td colspan=7/></tr></tfoot>');
+            for (var i = 0; i < this.firstDayOfWeek; i++) {
+                days.push(days.shift());
+            }
+
+            thead.insert('<tr><td class=title colspan=7/></tr>');
+            thead.insert('<tr><td class="cvbutton otherDay">«</td><td class="cvbutton otherDay">‹</td><td class="cvbutton today" colspan=3>'+
+                    this.locale.get('today') + '</td><td class="cvbutton otherDay">›</td><td class="cvbutton otherDay">»</td></tr>');
+            thead.insert('<tr>'+'<th/>'.times(7)+'</tr>');
+            thead.select('th').each(function(th, i) {
+                th.update(days[i]);
+            });
+            thead.on('click', '.cvbutton', onSelect.bind(this));
+
+            tbody.insert('<tr class=days><td/><td/><td/><td/><td/><td/><td/></tr>'.times(6));
+            tbody.on('click', 'td', onSelect.bind(this));
+
+            return content;
+        },
+
+        update: function($super, date) {
+            $super(date);
+
+            var table = this.datesTable;
+            table.select('.title')[0].update(this.locale.formatDate(date, '%B %Y'));
+
+            var buttons = table.select('.cvbutton'),
+                Y = date.getFullYear(), m = date.getMonth(), d = date.getDate();
+            // equivalent to getParts(3)
+            buttons[0].parts = [Y-1, m, d];
+            buttons[1].parts = [Y, m-1, d];
+            buttons[2].parts = new Date().getParts(3); // Date.now()
+            buttons[3].parts = [Y, m+1, d];
+            buttons[4].parts = [Y+1, m, d];
+
+            var d = new Date(date.getFullYear(), date.getMonth(), 1);
+            d.setDate(1 + this.firstDayOfWeek - d.getDay());
+            table.select('.days td').each(function(day) {
+                day.update(d.getDate())
+                    .toggleClassName('otherDay', d.getMonth() != date.getMonth())
+                    .parts = d.getParts(3);
+                this.updatePeriod(date, $R(d.valueOf(), d.setDate(d.getDate()+1), true), day);
+            }, this);
+
+            table.select('.days').each(function(week) {
+                var empty = week.select('td.otherDay').length == 7;
+                week.toggle(!empty);
+            });
+        }
+    };
+
+    // private Calendar.Dates method
+    function onSelect(event, td) {
+        if (td.parts) {
+            // cache classname because it's about to change
+            var otherDay = td.hasClassName('otherDay');
+            // set only date parts not time
+            this.update(new Date(this.date).setParts(td.parts));
+            if (!otherDay) {
+                this.setValue(this.date);
+            }
+        }
+    }
+
+    /*
+     * Set date values in order from year to millisecond.
+     */
+    Date.prototype.setParts = function(parts) {
+        var setters = $w('setYearOnly setMonthOnly setDate setHours setMinutes setSeconds setMilliseconds');
+        for (var i = 0; i < parts.length; i++) {
+            if (isFinite(parts[i])) {
+                this[setters[i]](parts[i]);
+            }
+        }
+        return this;
+    };
+
+    Date.prototype.getParts = function(num) {
+        var getters = $w('getFullYear getMonth getDate getHours getMinutes getSeconds getMilliseconds');
+        var parts = [];
+        for (var i = 0; i < num; i++) {
+            parts.push(this[getters[i]]());
+        }
+        return parts;
+    };
+
+})();
+
+(function(){
+    'use strict';
+
+    Calendar.Time = { /** @memberOf Calendar.Time */
+        // add standard time fields to default format
+        format: '%c',
+
+        getContent: function($super) {
+            var content = $super();
+            var fieldset = content.select('tfoot td')[0] || content.addTag('fieldset'),
+                hours = enumToSelect($R(0, 23)),
+                minutes = enumToSelect([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]);
+            fieldset.insert(hours);
+            fieldset.insert('<span> : </span>');
+            fieldset.insert(minutes);
+            fieldset.on('change', 'select', onChange.bind(this));
+
+            this.timeControls = [].concat(
+                hours.descendants().each(function(hour) {
+                    hour.parts = [,,, hour.value];
+                    hour.nextParts = [,,, (hour.value|0)+1];
+                }),
+                minutes.descendants().each(function(min) {
+                    min.parts = [,,,, min.value];
+                    min.nextParts = [,,,, (min.value|0)+5];
+                })
+            );
+
+            return content;
+        },
+
+        update: function($super, date) {
+            var d = new Date(0).setParts(date.getParts(4));
+            this.timeControls.each(function(option) {
+                var range = $R(
+                        new Date(d).setParts(option.parts),
+                        new Date(d).setParts(option.nextParts),
+                        true
+                );
+                this.updatePeriod(date, range, option);
+                if (range.include(date)) date.setParts(option.parts);
+            }, this);
+            $super(date);
+        }
+    };
+
+    // private Calendar.Time method
+    function onChange(event, select) {
+        var option = select.selectedOptions[0];
+        if (option.parts) {
+            this.update(new Date(this.date).setParts(option.parts));
+        }
+    }
+
+    function enumToSelect(enumer) {
+        var select = new Element('select');
+        enumer.each(function(value) {
+            select.addTag('option').update(value);
+        });
+        return select;
+    }
+
+})();
---
+++ src/widget.js
@@ -0,0 +1,227 @@
+/**
+ *  This file is part of CalendarView.
+ *
+ *  CalendarView is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CalendarView is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with CalendarView.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @copyright 2014 Daniel Deady <daniel@clockworkgeek.com>
+ */
+(function(){
+    'use strict';
+
+    /*
+     * A rather optimistic base type for all field-based widgets.
+     * 
+     * Constructor accepts multiple mixins which work the same as
+     * extending via Class.create().
+     * 
+     * Requires at least one `field` (as ID or element) in the constructor.
+     * `field` may also be an array of elements.
+     * 
+     * Requires one or more `control` classes too.
+     */
+    self.Widget = Class.create({ /** @memberOf Widget */
+
+        // individual elements may have their own formatting
+        formatAttr: 'data-format',
+
+        // default formatting for localised datetime fields
+        format: '%x',
+
+        containerClass: 'calendar',
+
+        extend: function(object) {
+            Object.keys(object).each(function(key) {
+                var value = object[key];
+                // based on Class#addMethods if you hadn't guessed
+                if (Object.isFunction(value) && value.argumentNames()[0] == '$super') {
+                    value = this[key].wrap(value);
+                }
+                this[key] = value;
+            }, this);
+        },
+
+        initialize: function() {
+            var t = this;
+            // copy all mixins/options to this object
+            $A(arguments).each(t.extend.bind(t));
+
+            // allow descendents to override locale, or specify language
+            t.locale = t.locale || new Locale(t.language);
+            // allow descendents to override formatting, default to using dates
+            t.formatFunc = t.formatFunc || t.locale.formatDate.bind(t.locale);
+            t.parseFunc = t.parseFunc || t.locale.parseDate.bind(t.locale);
+
+            // delay further initialization unless it's already been delayed
+            if (document.loaded) t.initializeElements();
+            else document.observe('dom:loaded', t.initializeElements.bind(t));
+        },
+
+        initializeElements: function() {
+            this.field = $(this.field);
+            if (!Object.isArray(this.field)) this.field = [this.field];
+            this.element = this.getContent();
+        },
+
+        getContent: function() {
+            return new Element('div', {'class': this.containerClass});
+        },
+
+        getFormat: function(field) {
+            return field.readAttribute(this.formatAttr) || this.format;
+        },
+
+        getValue: function() {
+            // return first truthy field
+            // might be null or undefined if something went wrong
+            var result = null;
+            this.field.each(function(field) {
+                try {
+                    var value = $F(field).trim();
+                }
+                catch (e) {
+                    value = field.innerHTML.trim();
+                }
+                if (result = this.parseFunc(value, this.getFormat(field))) {
+                    throw $break;
+                }
+            }, this);
+            return result;
+        },
+
+        setValue: function(value) {
+            this.field.each(function(field) {
+                value = this.formatFunc(value, this.getFormat(field));
+                try {
+                    field.setValue(value);
+                }
+                catch (e) {
+                    field.innerHTML = value;
+                }
+            }, this);
+        },
+
+        update: function(value) {
+            // a soft equivalent to setValue(), does not directly update field
+        }
+
+    });
+
+    /*
+     * Embeds itself in a parent container and does not close.
+     * `parent` must be passed to constructor.
+     * Sets field value immediately on every update.
+     */
+    Widget.Embedded = { /** @memberOf Widget.Embedded */
+        getContent: function($super) {
+            return $(this.parent).appendChild($super());
+        },
+
+        update: function($super, value) {
+            $super(value);
+            this.setValue(value);
+        }
+    };
+
+    /*
+     * Remains hidden until `trigger` element is clicked.
+     */
+    Widget.Popup = { /** @memberOf Widget.Popup */
+        initializeElements: function($super) {
+            $super();
+            var trigger = $(this.trigger) || this.field;
+            if (!Object.isArray(trigger)) trigger = [trigger];
+            trigger.invoke('observe', 'click', this.toggle.bind(this));
+            this.trigger = trigger;
+            Event.observe(window, 'resize', this.reposition.bind(this));
+        },
+
+        getContent: function($super) {
+            var element = $super().addClassName('popup');
+            document.body.insert(
+                this.parent = element.wrap('div').absolutize().hide()
+            );
+            if (self.Draggable) {
+                new Draggable(element);
+            }
+            self.test = element;
+            return element;
+        },
+
+        /*
+         * Show or hide. Equivalent to Element.toggle()
+         */
+        toggle: function(event) {
+            this.parent.toggle();
+            if (event instanceof UIEvent) this.relativeTo = event.findElement();
+            this.reposition();
+            // place main element somewhere convenient
+            var element = this.element,
+                elementSize = element.getDimensions(),
+                triggerSize = this.relativeTo.getDimensions(),
+                left = (triggerSize.width - elementSize.width)/2,
+                top = triggerSize.height,
+                maxBottom = Math.max(document.body.up().getHeight(),
+                        document.viewport.getScrollOffsets().top + document.viewport.getHeight());
+            if (this.parent.measure('top') + top + elementSize.height > maxBottom) {
+                top = -elementSize.height;
+            }
+            element.setStyle({left: left+'px', top: top+'px'});
+        },
+
+        /*
+         * Reposition parent only, which is an invisible wrapper.
+         * Position is relative to triggering element.
+         * 
+         * Parent can not reliably be a child of trigger as not all elements
+         * can be parents (<img>, <input>) and calendar doesn't want to deal
+         * with inherited styles (fonts, alignment, etc).
+         * 
+         * So repositioning is dealt with explicitly by observing resizes.
+         */
+        reposition: function() {
+            if (this.relativeTo) {
+                var position = this.relativeTo.cumulativeOffset();
+                this.parent.setStyle({left: position.left+'px', top: position.top+'px'});
+            }
+        }
+    };
+
+    /*
+     * Hide when a hard value is selected (eg. clicking on a date)
+     * or when clicking outside widget.
+     */
+    Widget.AutoHide = { /** @memberOf Widget.AutoHide */
+        toggle: function($super, event) {
+            $super(event);
+            var parent = this.parent,
+                triggers = this.trigger;
+            // hide() needs to be internal func so it is named and individual
+            function hide(event) {
+                if (!event.findElement().descendantOf(parent) && !triggers.include(event.findElement())) {
+                    parent.hide();
+                    document.stopObserving('mousedown', hide);
+                }
+            }
+            if (this.parent.visible()) {
+                document.observe('mousedown', hide);
+            }
+        },
+
+        setValue: function($super, value) {
+            $super(value);
+            this.parent.hide();
+        }
+    };
+
+})();
