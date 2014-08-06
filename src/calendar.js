e8b3ea60759058794b055773da6f22c84d81d222
Daniel Deady <daniel@clockworkgeek.com>
2014-08-06 16:13:06 +0100

    Tidy up license docs for closure compilation.

--- src/calendar.js
+++ src/calendar.js
@@ -1,25 +1,25 @@
 /**
- *  This file is part of CalendarView.
+ * This file is part of CalendarView.
  *
- *  CalendarView is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * CalendarView is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  CalendarView is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * CalendarView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with CalendarView.  If not, see <http://www.gnu.org/licenses/>.
- *  
- *  @copyright 2014 Daniel Deady <daniel@clockworkgeek.com>
- *  
- *  Based on previous work by, with thanks to, but without copying of:
- *  - Yuri Leikind
- *  - Justin Mecham <justin@aspect.net>
- *  - Mihai Bazon (author of original www.dynarch.com/projects/calendar)
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CalendarView.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2014 Daniel Deady <daniel@clockworkgeek.com>
+ * 
+ * Based on previous work by, with thanks to, but without copying of:
+ * - Yuri Leikind
+ * - Justin Mecham <justin@aspect.net>
+ * - Mihai Bazon (author of original www.dynarch.com/projects/calendar)
  */
 
 (function(){
